302b4d595119c94fd66d92143afbf907c717267c
AnotheerDev <leger.julien94@gmail.com>
2024-03-19 00:45:28 +0100

    Refactor Dashboard component to display multiple TicketCards

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,12 @@ import TicketCard from "./(components)/TicketCard";
 const Dashboard = () => {
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4"></div>
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
   );
 };
